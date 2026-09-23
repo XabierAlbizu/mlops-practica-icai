9a9adf54e8948dbd4413c5c2471cd0d149ad5280
XabierAlbizu <202207372@alu.comillas.edu>
2026-09-23 18:36:40 +0200

    train added

--- train.py
+++ train.py
@@ -14,11 +14,22 @@ y = iris.target
 
 # Iniciar un experimento de MLflow
 with mlflow.start_run():
-    # Dividir los datos en conjuntos de entrenamiento y prueba
+# Dividir los datos en conjuntos de entrenamiento y prueba
     X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.3, random_state=42
     )
     # Inicializar y entrenar el modelo
     model = RandomForestClassifier(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
+    # Realizar predicciones y calcular la precisión
     y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    # Guardar el modelo entrenado en un archivo .pkl
+    joblib.dump(model, 'model.pkl')
+    # Registrar el modelo con MLflow
+    mlflow.sklearn.log_model(model, "random-forest-model")
+    # Registrar parámetros y métricas
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_metric("accuracy", accuracy)
+    print(f"Modelo entrenado y precisión: {accuracy:.4f}")
+    print("Experimento registrado con MLflow.")
